335011a7963845ad6e94f71551e04c59b7f72b5b
Vinícius Pessoa <vini.pessoa7@gmail.com>
2019-08-19 09:28:33 -0400

    feat: merge.cpp: merge-sort funcionando + avaliação com 100000 números.

--- merge.cpp
+++ merge.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <cstdlib>
+#include <time.h>
+#include <cmath>
 
 using namespace std;
 
@@ -20,38 +22,219 @@ int numeroAleatorio(int min, int max){
 }
 
 /*
+* Embaralha Vetor
+* Escolhe uma posição aleatoria do vetor e troca com a da vez do loop.
+*/
+void embaralhaVetor(int* vet, int tam, int min, int max){
+    for (int i = 0; i < tam; i++){
+        int rnd = numeroAleatorio(min, max);
+
+        int aux = vet[i];
+        vet[i] = vet[rnd];
+        vet[rnd] = aux;
+    }
+}
+
+/*
 * Gera números aleatórios distintos e adiciona-os no vetor.
-* @param qtNum é a quantidade de números a serem gerados.
-* @param min é o valor mínimo que um número poderá ter.
-* @param max é o valor máximo que um número poderá ter.
+* @param qtNum  Quantidade de números a serem gerados.
+* @param min  Valor mínimo que um número poderá ter.
+* @param max  Valor máximo que um número poderá ter.
 */
 int* numerosAleatorios(int qtNum, int min, int max){
     int* vet = new int[qtNum]; //vetor que será retornado.
-    
+
+    // Cria um vetor ordenado
     for (int i = 0; i < qtNum; i++){
-        int num = numeroAleatorio(min, max);
-
-        //Checar se numero ja existe no vetor.
-        for (int j = 0; j <= i; j++){
-            if (vet[j] == num) {
-                i--;
-                break; // para o loop caso encontre o número no vetor.
-            }
-
-            //Adicionar numero no vetor.
-            if (j == i){
-                vet[i] = num;
-            }
-        }
+        vet[i+min] = i;
     }
+
+    // Embaralha o vetor
+    embaralhaVetor(vet, qtNum, min, max);
 
     return vet;
 
 }
 
+template <class T>
+void imprimeVetor(T* vet, int n){
+    cout << vet[0];
+    for (int i = 1; i < n; i++){
+        cout << ", " << vet[i];
+    }
+    cout << endl;
+}
+
+/*
+* Algoritmo que recebe duas partes de um único vetor e junta-as noutro vetor auxiliar de forma ordenada.
+* @param v  O vetor dividido que será fundido.
+* @param inicio  Posição inicial do vetor
+* @param meio  Posição mediana do vetor, que divide o vetor em dois.
+* @param fim  Posição final do vetor.
+* @param modo  Modo de ordenação do vetor. (0-Crescente / 1-Decrescente).
+*/
+void merge(int* v, int inicio, int meio, int fim, int modo) {
+    int *aux; // Vetor auxiliar 
+    int p1, p2; // Posição de verificação dos vetores. (index)
+    int tamanho; // tamanho do vetor
+    bool fim1 = false, fim2 = false; // indica o fim dos vetores.
+
+    tamanho = fim - inicio + 1;
+    aux = new int[tamanho];
+    p1 = inicio;
+    p2 = meio + 1;
+
+    if (aux != NULL){
+        for (int i = 0; i < tamanho; i++){
+            if (modo == 0){ // Ordena em ordem crescente
+                if (!fim1 && !fim2){
+                    if (v[p1] < v[p2]){
+                        aux[i] = v[p1++];
+                    } else {
+                        aux[i] = v[p2++];
+                    }
+                    if (p1 > meio) {
+                        fim1 = true;
+                    }
+                    if (p2 > fim) {
+                        fim2 = true;
+                    }
+                } else {
+                    if (!fim1){
+                        aux[i] = v[p1++];
+                    } else {
+                        aux[i] = v[p2++];
+                    }
+                }
+            } else { // Ordena em ordem decrescente
+                if (!fim1 && !fim2){
+                    if (v[p1] > v[p2]){
+                        aux[i] = v[p1++];
+                    } else {
+                        aux[i] = v[p2++];
+                    }
+                    if (p1 > meio) {
+                        fim1 = true;
+                    }
+                    if (p2 > fim) {
+                        fim2 = true;
+                    }
+                } else {
+                    if (!fim1){
+                        aux[i] = v[p1++];
+                    } else {
+                        aux[i] = v[p2++];
+                    }
+                }
+            }
+            
+        }
+
+        // copia os valores do vetor auxiliar para o vetor passado como parâmetro.
+        for (int i = 0; i < tamanho; i++){
+            v[i+inicio] = aux[i];
+        } 
+
+    }
+
+    delete(aux);
+
+}
+
+//Merge Sort
+void mergeSort(int* vet, int inicio, int fim, int modo) {
+    if (inicio < fim){
+        int meio = floor((fim + inicio)/2); // calcula a metade do vetor.
+        mergeSort(vet, inicio, meio, modo); // Aplica algoritmo recursivamente na primeira metade do vetor.
+        mergeSort(vet, meio+1, fim, modo); // Aplica algoritmo recursivamente na segunda metade do vetor.
+        merge(vet, inicio, meio, fim, modo); // junta as duas metádes do vetor, de acordo com o modo.
+    }
+}
+
+// Avalia o tempo de execução do algoritmo Merg-Sort com 100.000 números ordenados de formas distintas
+void avaliar() {
+    unsigned int n = 100000;
+    int op = 5; // numero de operações
+    double* tempo = new double[op]; // Vetor que armazena os tempos de execução
+
+    //Alocação dos vetores auxiliares
+    int** vetores = new int*[op];
+    for (int i = 0; i < op; i++){
+        vetores[i] = new int[n];
+    }
+
+    //Vetor desordenado
+    vetores[0] = numerosAleatorios(n, 0, n);
+
+    //Copia o primeiro vetor para os demais
+    for (int i = 1; i < op; i++){
+        for (int j = 0; j < n; j++){
+            vetores[i][j] = vetores[0][j];
+        }
+    }
+    
+    //Vetor Decrescente
+    mergeSort(vetores[1], 0, n, 1);
+
+    //Vetor Crescente
+    mergeSort(vetores[2], 0, n, 0);
+
+    //Vetor Crescente/Decrescente
+    mergeSort(vetores[3], 0, n/2, 0); // ordena a primeira metade do vetor em ordem crescente
+    mergeSort(vetores[3], (n/2)+1, n, 1); // ordena a segunda metade do vetor em ordem decrescente
+
+    // Vetor Decrescente/Crescente
+    mergeSort(vetores[4], 0, n/2, 1); // ordena a primeira metade do vetor em ordem decrescente
+    mergeSort(vetores[4], (n/2)+1, n, 0); // ordena a segunda metade do vetor em ordem crescente
+
+    for (int i = 0; i < op; i++){
+        clock_t tick_inicio, tick_fim; 
+        
+        tick_inicio = clock(); // Salva o momento antes da execução do algoritmo.
+        mergeSort(vetores[i], 0, n, 0);
+        tick_fim = clock(); // Salva o momento após a execução do algoritmo.
+
+        tempo[i] = (tick_fim - tick_inicio) * 1000.0 / CLOCKS_PER_SEC; // Calcula o tempo 
+
+    }   
+
+    // Impressão dos tempos de execução
+    for (int i = 0; i < op; i++){
+        cout << "Tempo gasto (";
+        switch(i){
+            case 0:
+                cout << "Desordenado";
+                break;
+            case 1:
+                cout << "Decrescente";
+                break;
+            case 2:
+                cout << "Crescente";
+                break;
+            case 3:
+                cout << "Crescente/Decrescente";
+                break;
+            case 4:
+                cout << "Decrescente/Crescente";
+                break;
+        }
+        printf("): %g ms.\n", tempo[i]);
+
+    }
+
+    // Desalocação de recursos
+    for (int i = 0; i < op; i++){
+        delete(vetores[i]);
+    }
+    delete(vetores);
+    delete(tempo);
+
+}
+
+// Main
 int main (){
 
-
+    avaliar();
 
     return 0;
 }
