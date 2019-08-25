9e854fd3ca9624a9820acad63ae3af16f9c19aa4
Vinícius Pessoa <vini.pessoa7@gmail.com>
2019-08-25 16:07:57 -0400

    feat: mergeSort.py: ordenação decrescente

--- Python/mergeSort.py
+++ Python/mergeSort.py
@@ -1,30 +1,44 @@
 import random
-def mergeSort(vet):
+def mergeSort(vet, inv):
     if len(vet) <= 1:
         return vet
     meio = int(len(vet)/2)
-    a = mergeSort(vet[:meio])
-    b = mergeSort(vet[meio:])
+    a = mergeSort(vet[:meio], inv)
+    b = mergeSort(vet[meio:], inv)
 
-    vet = merge(a, b)
+    vet = merge(a, b, inv)
     return vet
 
-def merge(a, b):
+def merge(a, b, inv):
     vet = []
     indexA = 0
     indexB = 0
     while indexA < len(a) and indexB < len(b):
-        if a[indexA] < b[indexB]:
-            vet.append(a[indexA])
-            indexA += 1
-        elif a[indexA] > b[indexB]:
-            vet.append(b[indexB])
-            indexB += 1
+        if not inv:
+            if a[indexA] < b[indexB]:
+                vet.append(a[indexA])
+                indexA += 1
+            elif a[indexA] > b[indexB]:
+                vet.append(b[indexB])
+                indexB += 1
+            else:
+                vet.append(a[indexA])
+                vet.append(b[indexB])
+                indexA += 1
+                indexB += 1
         else:
-            vet.append(a[indexA])
-            vet.append(b[indexB])
-            indexA += 1
-            indexB += 1
+            if a[indexA] > b[indexB]:
+                vet.append(a[indexA])
+                indexA += 1
+            elif a[indexA] < b[indexB]:
+                vet.append(b[indexB])
+                indexB += 1
+            else:
+                vet.append(a[indexA])
+                vet.append(b[indexB])
+                indexA += 1
+                indexB += 1
+
 
     while indexA < len(a):
         vet.append(a[indexA])
@@ -38,11 +52,9 @@ def merge(a, b):
 
 
 vet = []
-for i in range(random.randint(15, 20)):
-    vet.append(random.randint(0,30))
-print(mergeSort(vet))
-
-vet = []
-for i in range(random.randint(0, 5)):
-    vet.append(random.randint(0,30))
-print(mergeSort(vet))
+for i in range(100):
+    vet.append(random.randint(0,100))
+print("Vetor desordenado:")
+print(vet)
+print("Vetor ordenado:")
+print(mergeSort(vet, True))
